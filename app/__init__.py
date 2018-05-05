4530b9786f4b99048ecb58bab952e61d61827d3d
Ivan <io@deppa.ru>
2018-05-05 22:22:33 +0300

    7. Error pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import logging
+import os
+from logging.handlers import SMTPHandler, RotatingFileHandler
+
 from flask import Flask
 from flask_uploads import UploadSet, IMAGES, configure_uploads
 
@@ -15,4 +19,31 @@ login.login_view = 'login'
 photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
 
-from app import routes, models
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Ошибка на сервере',
+            credentials=auth, secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/films.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)s]'))
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Запуск сервера')
+
+from app import routes, models, errors
