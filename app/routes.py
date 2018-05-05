b6f1deb91a68f281e8aff5e8b41b0a6ce3bcc67d
Ivan <io@deppa.ru>
2018-05-05 22:13:32 +0300

    6. Profile

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,22 @@
-from flask import render_template, flash, url_for
-from flask_login import current_user, login_user, logout_user
+from datetime import datetime
+from flask import render_template, flash, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.utils import redirect
 
 from app import app, db
-from app.forms import LoginForm, RegisterForm
+from app.forms import LoginForm, RegisterForm, EditProfileForm
 from app.models import User
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'ivan'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -62,3 +68,30 @@ def register():
         return redirect(url_for('login'))
 
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first()
+    posts = [
+        {'author': user, 'body': 'Тестовое описание фильма1'},
+        {'author': user, 'body': 'Тестовое описание фильма2'},
+    ]
+    return render_template('user.html', title='Профиль пользователя', user=user, posts=posts)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Изменения профиля сохранены')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile', title='Редактирование профиля', form=form)
